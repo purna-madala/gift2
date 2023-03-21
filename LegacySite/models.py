4bb41718a5861f2ee1caac14fa42937b8336180c
Purna Sai Madala <psm2952@nyu.edu>
2023-03-21 19:37:46 -0400

    part 2
    Signed-off-by: Purna Sai Madala <psm2952@nyu.edu>

--- LegacySite/models.py
+++ LegacySite/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.backends import BaseBackend
 from . import extras
+from fernet_fields import EncryptedTextField
 
 # Create your models here.
 class User(AbstractBaseUser):
@@ -37,9 +38,9 @@ class Product(models.Model):
 
 class Card(models.Model):
     id = models.AutoField(primary_key=True)
-    data = models.BinaryField(unique=True)
+    data = EncryptedTextField()
     product = models.ForeignKey('LegacySite.Product', on_delete=models.CASCADE, default=None)
     amount = models.IntegerField()
-    fp = models.CharField(max_length=100, unique=True)
+    fp = EncryptedTextField()
     user = models.ForeignKey('LegacySite.User', on_delete=models.CASCADE)
     used = models.BooleanField(default=False)
